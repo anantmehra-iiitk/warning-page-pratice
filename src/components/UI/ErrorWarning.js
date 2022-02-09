d1cee44796399f6771a1ac44a20e9b0ebee2c118
anant mehra <anant0007mehra@gmail.com>
2022-02-09 13:14:37 +0530

    Portal Use

--- src/components/UI/ErrorWarning.js
+++ src/components/UI/ErrorWarning.js
@@ -30,13 +30,13 @@ const ErrorWarning = (props) => {
   return (
     <React.Fragment>
       {ReactDOM.createPortal(
-        <Backdrop onClick={props.onConfirm} />,
+        <Backdrop onConfirm={props.onConfirm} />,
         document.getElementById("backdrop-root")
       )}
       {ReactDOM.createPortal(
         <WarningOverlay
           title={props.title}
-          message={props.messagr}
+          message={props.message}
           onConfirm={props.onConfirm}
         />,
         document.getElementById("overlay-root")
